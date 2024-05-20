4779ed9e4b22b975f9d6111f15b8f78da94c5075
Developer Shyam <shyammishra5857@gmail.com>
2024-05-20 15:57:02 +0530

    major changes

--- src/app/blog/[slug]/page.js
+++ src/app/blog/[slug]/page.js
@@ -1,10 +1,7 @@
 import SingleBlog from '@/components/SingleBlog';
-import Link from 'next/link'
-// import { useParams } from 'next/navigation';
 
 const page = ({params}) => {
   const {slug} = params;
-  // console.log(slug,'slug'); 
   return (
     <>
       <SingleBlog slug= {slug}/>   
